d9e5fc35b11574b4cd856f610fbe8cc74b6ed766
Jordan De Dapper <jordan.dedapper@gmail.com>
2018-12-11 16:21:00 +0100

    Entity
    Entité

---
+++ site-de-reflexion/src/Entity/Idees.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\IdeesRepository")
+ */
+class Idees
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Text;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->Text;
+    }
+
+    public function setText(string $Text): self
+    {
+        $this->Text = $Text;
+
+        return $this;
+    }
+}
---
+++ site-de-reflexion/src/Repository/IdeesRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Idees;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Idees|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Idees|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Idees[]    findAll()
+ * @method Idees[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class IdeesRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Idees::class);
+    }
+
+    // /**
+    //  * @return Idees[] Returns an array of Idees objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('i.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Idees
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
